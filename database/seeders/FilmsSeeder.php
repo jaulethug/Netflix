f13a2a6f4c169804f8e7cdcfb4cfd40d5a71142a
jaulethug <xjeremiedrx@gmail.com>
2023-11-06 12:51:59 -0500

    ajout de seeder et de migrations

--- database/seeders/FilmsSeeder.php
+++ database/seeders/FilmsSeeder.php
@@ -43,6 +43,76 @@ class FilmsSeeder extends Seeder
                 'rating' => 4,
                 'lien' => 'lien du video3'
             ],
+            [
+                'id' => 4,
+                'titre' => 'john wick',
+                'resume'=> 'john wick tue tout le monde',
+                'pochette' => 'image du film4',
+                'duree' =>  "210 minutes",
+                "annee" => 2004,
+                'rating' => 5,
+                'lien' => 'lien du video4'
+            ],
+            [
+                'id' => 5,
+                'titre' => 'john wick 2',
+                'resume'=> 'john wick tue tout le monde encore',
+                'pochette' => 'image du film5',
+                'duree' =>  "231 minutes",
+                "annee" => 2005,
+                'rating' => 3,
+                'lien' => 'lien du video5'
+            ],
+            [
+                'id' => 6,
+                'titre' => 'john wick 3',
+                'resume'=> 'john wick tue tout le monde encore encore',
+                'pochette' => 'image du film6',
+                'duree' =>  "120 minutes",
+                "annee" => 2005,
+                'rating' => 5,
+                'lien' => 'lien du video6'
+            ],
+            [
+                'id' => 7,
+                'titre' => 'pirates des caraibes',
+                'resume'=> 'jack sparrow le pirate',
+                'pochette' => 'image du film5',
+                'duree' =>  "210 minutes",
+                "annee" => 2004,
+                'rating' => 5,
+                'lien' => 'lien du video7'
+            ],
+            [
+                'id' => 8,
+                'titre' => 'shrek',
+                'resume'=> 'shrek logre fait du grabuge',
+                'pochette' => 'image du film8',
+                'duree' =>  "200 minutes",
+                "annee" => 2004,
+                'rating' => 5,
+                'lien' => 'lien du video8'
+            ],
+            [
+                'id' => 9,
+                'titre' => 'shrek 2',
+                'resume'=> 'le retour de shrek',
+                'pochette' => 'image du film9',
+                'duree' =>  "100 minutes",
+                "annee" => 2008,
+                'rating' => 5,
+                'lien' => 'lien du video9'
+            ],
+            [
+                'id' => 10,
+                'titre' => 'shrek 3',
+                'resume'=> 'les aventures de shrek',
+                'pochette' => 'image du film10',
+                'duree' =>  "190 minutes",
+                "annee" => 2004,
+                'rating' => 5,
+                'lien' => 'lien du video10'
+            ],
             ]);
             
     }
