0fc955cae5e9c206de8f95478683a3330321d5fa
BelderialSerperod <belderialserperod@gmail.com>
2023-11-16 15:27:26 -0500

    test pour savoir quel git

--- app/Models/Film.php
+++ app/Models/Film.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+//test
 class Film extends Model
 {
     protected $fillable = ['titre', 'resume', 'pochette', 'duree', 'annee', 'rating', 'lien'];
