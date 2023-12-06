e09566abc2aab7034a0cb5cff0170457b300de29
jaulethug <78559941+jaulethug@users.noreply.github.com>
2023-12-05 22:22:44 -0500

    fait page creation, manque encore a la link a kkch

--- resources/views/films/create.blade.php
+++ resources/views/films/create.blade.php
@@ -1,8 +1,22 @@
 @extends('layouts.app')
-@section('title', 'create movie')
+@section('title', 'ajout de film')
 @section('contenu')
 <h1>formulaire creation film</h1>
-<form>
-
-
+<form method="POST" action="">
+<label for="titre">Titre du film</label><br>
+<input type="text" name="titre"><br>
+<label for="">Resume du film</label><br>
+<input type="text" name="resume"><br>
+<label for="image">Image du film</label><br>
+<input type="text" name="pochette"><br>
+<label for="duree">Duree du film</label><br>
+<input type="text" name="duree"><br>
+<label for="annee">Annee de creation du film</label><br>
+<input type="text" name="annee"><br>
+<label for="rating">Note du film</label><br>
+<input type="text" name="rating"><br>
+<label for="lien">Bande annonce du film</label><br>
+<input type="text" name="lien"><br>
+<button type="submit">termine</button>
+</form>
 @endsection
